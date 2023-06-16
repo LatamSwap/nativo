e404df0b3a920f78bde3acfab1112e472c2ee0cd
eugenioclrc <eugenioclrc@gmail.com>
2023-06-16 19:11:06 -0300

    add banner

--- test/helpers/AddressSet.sol
+++ test/helpers/AddressSet.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.13;
+// Based on https://github.com/horsefacts/weth-invariant-testing
+// @author horsefactspragma solidity ^0.8.13;
 
 struct AddressSet {
     address[] addrs;
