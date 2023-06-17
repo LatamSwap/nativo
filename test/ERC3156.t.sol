515d8fff9b571bbf80575f08e3d829af72ebdb14
eugenioclrc <eugenioclrc@gmail.com>
2023-06-17 15:35:48 -0300

    template test ERC3156

---
+++ test/ERC3156.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10;
+
+import "forge-std/Test.sol";
+
+import {Nativo} from "src/Nativo.sol";
+
+// FLASHLOAN tests
+contract ERC3156Test is Test {
+    Nativo public nativo;
+    address immutable public EOA = makeAddr("EOA");
+
+    function setUp() {
+        // name and symbol depend on the blockchain we are deploying
+        nativo = new Nativo("Wrapped Native crypto", "wANY");
+    }
+}
