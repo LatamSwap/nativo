e20af42d70a63632cc593b872247dbdee6088a06
eugenioclrc <eugenioclrc@gmail.com>
2023-06-24 16:29:10 -0300

    fix interface namming

--- src/INativo.sol
+++ src/INativo.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.4;
 
-interface Nativo {
+interface INativo {
     error AddressZero();
     error AllowanceOverflow();
     error AllowanceUnderflow();
